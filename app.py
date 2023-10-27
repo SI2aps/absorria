1d138c650061563096cbc506904bdbbadcb0cf54
Danielle Silva Pereira <105320467+DanielleSilvaPereira@users.noreply.github.com>
2023-10-27 14:12:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,30 @@ def calculadora():
 total_absorvente = 0
 conversao_carbono_coletor = 0
 conversao_carbono_descartavel = 0
+comparacao_descartavel = 0 
 
-@app.route('/', methods=['GET', 'POST'])
-def form_calculadora():
-    if request.method == 'POST':
-        qtdade = int(Form.qtdade_absorvente.data)
-        dias = int(FormField.dias_absorvente.data)
+@app.route('/calculadora', methods=['GET', 'POST'])
+def calculadora():
+    form = ReusableForm(request.form)
+    total_absorvente = 0
+    conversao_carbono_coletor = 0
+    conversao_carbono_descartavel = 0
+    comparacao_descartavel = 0 
+
+    if request.method == 'POST' and form.validate():
+        qtdade = int(form.qtdade_absorvente.data)
+        dias = int(form.dias_absorvente.data)
         total_absorvente = qtdade * dias
         conversao_carbono_descartavel = (total_absorvente * 20.19) / 195
         conversao_carbono_coletor = total_absorvente * 1.77
+        comparacao_descartavel = (total_absorvente * 100) / 73428
 
-    return render_template('resultado.html')
+    return render_template('calculadora.html',
+                           total_absorvente=total_absorvente,
+                           conversao_carbono_descartavel=conversao_carbono_descartavel,
+                           conversao_carbono_coletor=conversao_carbono_coletor,
+                           comparacao_descartavel=comparacao_descartavel,
+                           form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
